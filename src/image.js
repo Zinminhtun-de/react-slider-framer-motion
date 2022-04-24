67b10644258ad5f6dc835414fb7ace43e2679126
zinminhtun <zinminhtunok591997@gmail.com>
2022-04-24 14:01:54 +0630

    build react-slider-framer-motion

---
+++ src/image.js
@@ -0,0 +1,7 @@
+import image1 from "./assets/images/1.jpg";
+import image2 from "./assets/images/2.jpg";
+import image3 from "./assets/images/3.jpg";
+import image4 from "./assets/images/4.jpg";
+import image5 from "./assets/images/5.jpg";
+
+export default [image1, image2, image3, image4, image5];
